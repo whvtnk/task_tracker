13dd9abff24f2846b8c72db44b0c66fcad4a8817
whvtnk <ulan.tokan@icloud.com>
2026-02-19 00:43:19 +0500

    адинга аналитика костым

--- config/urls.py
+++ config/urls.py
@@ -17,13 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from tasks.views import TaskViewSet
+from tasks.views import TaskViewSet, AnalyticsView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/analytics/', AnalyticsView.as_view(), name='analytics'),
+
     path('api/', include(router.urls)),
 
     path('api-auth/', include('rest_framework.urls')),
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,13 @@
 from django.db import models
-from rest_framework import viewsets, filters
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+
+from django.contrib.auth.models import User
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q, Count
+
+from rest_framework import viewsets, filters
 from .models import Task
 from .serializers import TaskSerializer 
 from .permissions import IsAdminOrOwner
@@ -22,4 +29,33 @@ class TaskViewSet(viewsets.ModelViewSet):
         if user.is_superuser:
             return Task.objects.all().order_by('-created_at')
         return Task.objects.filter(models.Q(author=user) | models.Q(assignee=user)).order_by('-created_at')
+    
+
+class AnalyticsView(APIView):
+    permissioon_classes = [IsAdminUser]
+
+    def get(self, request):
+        total_tasks = Task.objects.count()
+        completed_tasks = Task.objects.filter(status='completed').count()
+        overdue_tasks = Task.objects.filter(status='overdue').count()
+
+
+        completion_rate = 0
+        if total_tasks > 0:
+            completion_rate = round((completed_tasks / total_tasks) * 100, 2)
+
+
+        users_stats = User.objects.annotate(
+            total_assigned=Count('assigned_tasks'),
+            completed=Count('assigned_tasks', filter=Q(assigned_tasks__status='completed')),
+            overdue=Count('assigned_tasks', filter=Q(assigned_tasks__status='overdue'))
+        ).values('username', 'total_assigned', 'completed', 'overdue')
+
+        return Response({
+            'total_tasks': total_tasks,
+            'completed_tasks': completed_tasks,
+            'overdue_tasks': overdue_tasks,
+            'completion_rate_percent': completion_rate,
+            'employee_stats': list(users_stats)
+        })
     
