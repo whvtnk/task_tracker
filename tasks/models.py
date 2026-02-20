49a97e4aec369d41a512dbdd2138e3fd50aad918
whvtnk <ulan.tokan@icloud.com>
2026-02-21 00:56:51 +0500

    Измеенил именование на агл

--- tasks/models.py
+++ tasks/models.py
@@ -10,38 +10,33 @@ class Task(models.Model):
     ]
 
     PRIORITY_CHOICES = [
-        (1, 'Low (1)'),
-        (2, 'Medium-Low (2)'),
-        (3, 'Medium (3)'),
-        (4, 'Medium-High (4)'),
-        (5, 'High (5)'),    
+        (1, 'Low'),
+        (2, 'Medium-Low'),
+        (3, 'Medium'),
+        (4, 'Medium-High'),
+        (5, 'High'),    
     ]
 
-    estimated_hours = models.IntegerField(verbose_name="Предполагаемые часы", default=0)
+    estimated_hours = models.IntegerField(verbose_name="Estimated hours", default=0)
 
-    title = models.CharField(max_length=200, verbose_name="Название")
-    description = models.TextField(verbose_name="Описание", blank=True)
+    title = models.CharField(max_length=200, verbose_name="Title")
+    description = models.TextField(verbose_name="Description", blank=True)
     
-    # Пока сделаем проект просто текстовым полем. 
-    # (В будущем можно создать отдельную таблицу Project, если захочешь)
-    project = models.CharField(max_length=100, verbose_name="Проект")
+    project = models.CharField(max_length=100, verbose_name="Project")
 
-    # Связи с пользователями
-    # on_delete=models.CASCADE означает: если удалят пользователя, удалятся и его задачи
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tasks', verbose_name="Автор")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tasks', verbose_name="Author")
     
-    # null=True, blank=True означает, что исполнителя может пока не быть
-    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks', verbose_name="Исполнитель")
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks', verbose_name="Assignee")
 
-    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=1, verbose_name="Приоритет")
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name="Статус")
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=1, verbose_name="Priority")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name="Status")
     
-    deadline = models.DateTimeField(verbose_name="Дедлайн")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    deadline = models.DateTimeField(verbose_name="Deadline")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
 
 
     def __str__(self):
         return f"{self.title} ({self.get_status_display()})"
     class Meta:
-        verbose_name = "Задача"
-        verbose_name_plural = "Задачи"
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
